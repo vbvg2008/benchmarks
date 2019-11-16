ca9c16dd59f99a9d32b1642a9f2567730c8cfd8b
vbvg2008 <shawnmengdong@gmail.com>
2019-11-16 23:15:06 +0000

    add data loader

---
+++ tutorials/t6_dataset_dataloader.py
@@ -0,0 +1,11 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch
+from skimage import io, transform
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms, utils
+
+https://download.pytorch.org/tutorial/faces.zip
