5662f535406768905caf86b783c720a5f2159705
vbvg2008 <shawnmengdong@gmail.com>
2019-12-18 23:13:05 -0700

    finished cross entropy loss

--- fe_future_test/test2_ce.py
+++ fe_future_test/test2_ce.py
@@ -1,23 +1,32 @@
-import tensorflow as tf
-import torch
-import numpy as np
 import pdb
 
+import numpy as np
+import tensorflow as tf
+import torch
 
-def compare_ce(batch_size, num_classes):
+
+def compare_ce(batch_size, num_classes, softmaxed=True):
     y_pred_source = np.random.rand(batch_size, num_classes)
+    if softmaxed:
+        batch_sum = np.reshape(np.sum(y_pred_source, axis=-1), (-1, 1))
+        y_pred_source = y_pred_source / np.tile(batch_sum, (1, num_classes))
     y_gt_source = np.random.randint(0, num_classes, size=(batch_size))
 
     y_pred_tf = tf.convert_to_tensor(y_pred_source)
     y_gt_tf = tf.convert_to_tensor(y_gt_source)
-    ce_tf_1a = tf.losses.sparse_categorical_crossentropy(y_gt_tf, y_pred_tf)
+    ce_tf_1a = tf.losses.sparse_categorical_crossentropy(y_gt_tf, y_pred_tf, from_logits=not softmaxed)
     print("ce_tf: {}".format(ce_tf_1a))
 
     y_pred_torch = torch.tensor(y_pred_source)
     y_gt_torch = torch.tensor(y_gt_source, dtype=torch.long)
-    # pdb.set_trace()
-    ce_torch_1a = torch.nn.CrossEntropyLoss(reduction="none")(y_pred_torch, y_gt_torch)
+    if not softmaxed:
+        ce_torch_1a = torch.nn.CrossEntropyLoss(reduction="none")(y_pred_torch, y_gt_torch)
+    else:
+        ce_torch_1a = torch.nn.NLLLoss(reduction="none")(torch.log(y_pred_torch), y_gt_torch)
     print("ce_torch: {}".format(ce_torch_1a))
+    # pdb.set_trace()
+
 
 #case 1: binary cross-entropy
-compare_ce(batch_size=10, num_classes=10)
+compare_ce(batch_size=4, num_classes=10, softmaxed=True)
+compare_ce(batch_size=4, num_classes=10, softmaxed=False)
