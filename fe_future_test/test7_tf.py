d1e18a0ece45c39069bf9ccd9e590e19a5728a68
vbvg2008 <shawnmengdong@gmail.com>
2020-01-01 06:44:22 -0700

    update test

--- fe_future_test/test7_tf.py
+++ fe_future_test/test7_tf.py
@@ -1,18 +1,21 @@
 """test case for tensorflow users
 """
-import tensorflow as tf
-import fastestimator as fe
-from tensorflow.python.keras import Sequential, layers
-from fastestimator.op.tensorop.model import ModelOp, UpdateOp
-from fastestimator.op.tensorop.loss import CrossEntropy
-from fastestimator.trace.metric import Accuracy
 import numpy as np
+import tensorflow as tf
+from tensorflow.python.keras import Sequential, layers
+
+import fastestimator as fe
+from fastestimator.op.tensorop.loss import CrossEntropy
+from fastestimator.op.tensorop.model import ModelOp, UpdateOp
+from fastestimator.trace.metric import Accuracy
+
 
 def Scale(dataset):
     dataset["x"] = tf.cast(dataset["x"], tf.float32)
     dataset["y"] = tf.cast(dataset["y"], tf.int32)
     dataset["x"] = dataset["x"] / 255.0
     return dataset
+
 
 def get_dataset(x, y, shuffle=True):
     data = {"x": x, "y": y}
@@ -24,6 +27,7 @@ def get_dataset(x, y, shuffle=True):
     ds = ds.batch(32)
     ds = ds.prefetch(1)
     return ds
+
 
 def tf_model(input_shape=(28, 28, 1), classes=10):
     model = Sequential()
@@ -41,19 +45,20 @@ def tf_model(input_shape=(28, 28, 1), classes=10):
 def get_estimator():
     #step 1
     (x_train, y_train), (x_eval, y_eval) = tf.keras.datasets.mnist.load_data()
-    pipeline = fe.Pipeline(train_data=get_dataset(x=np.expand_dims(x_train, -1), y=y_train), 
+    pipeline = fe.Pipeline(train_data=get_dataset(x=np.expand_dims(x_train, -1), y=y_train),
                            eval_data=get_dataset(x=np.expand_dims(x_eval, -1), y=y_eval),
                            batch_size=32)
     #step 2
     model = fe.build(model_def=tf_model, optimizer_def="adam")
-    network = fe.Network(ops=[ModelOp(model=model, inputs="x", outputs="y_pred"), 
-                              CrossEntropy(inputs=("y_pred", "y"), outputs="ce"),
-                              UpdateOp(model=model, loss_name="ce")])
+    network = fe.Network(ops=[
+        ModelOp(model=model, inputs="x", outputs="y_pred"),
+        CrossEntropy(inputs=("y_pred", "y"), outputs="ce"),
+        UpdateOp(model=model, loss_name="ce")
+    ])
     #step 3
     estimator = fe.Estimator(pipeline=pipeline,
                              network=network,
                              epochs=2,
                              steps_per_epoch=1875,
-                             traces=Accuracy(true_key="y", pred_key="y_pred"),
-                             monitor_names="ce")
+                             traces=Accuracy(true_key="y", pred_key="y_pred"))
     return estimator
