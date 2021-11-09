e3b92fe514723650cf8faa8c19a3c2b66e4468f6
vbvg2008 <shawnmengdong@gmail.com>
2021-11-09 23:33:58 +0000

    finished solov2 architecture

--- solov2/solov2_tf.py
+++ solov2/solov2_tf.py
@@ -1,6 +1,8 @@
 import pdb
 
+import numpy as np
 import tensorflow as tf
+import tensorflow_addons as tfa
 from tensorflow.keras import layers, regularizers
 
 
@@ -21,8 +23,83 @@ def fpn(C2, C3, C4, C5, l2_reg=1e-4):
     P4 = layers.Conv2D(256, kernel_size=3, padding="same", kernel_regularizer=regularizers.l2(l2_reg))(P4)
     P3 = layers.Conv2D(256, kernel_size=3, padding="same", kernel_regularizer=regularizers.l2(l2_reg))(P3)
     P2 = layers.Conv2D(256, kernel_size=3, padding="same", kernel_regularizer=regularizers.l2(l2_reg))(P2)
-    P6 = layers.MaxPool2D()(P5)
-    return P2, P3, P4, P5, P6
+    return P2, P3, P4, P5
+
+
+def pad_with_coord(inputs):
+    batch_size, height, width, _ = tf.shape(inputs)
+    x_ranges = tf.cast(tf.linspace(-1, 1, num=width), inputs.dtype)
+    y_ranges = tf.cast(tf.linspace(-1, 1, num=height), inputs.dtype)
+    X, Y = tf.meshgrid(x_ranges, y_ranges)
+    X = tf.tile(X[tf.newaxis, ..., tf.newaxis], [batch_size, 1, 1, 1])
+    Y = tf.tile(Y[tf.newaxis, ..., tf.newaxis], [batch_size, 1, 1, 1])
+    outputs = tf.concat([inputs, X, Y], axis=-1)
+    return outputs
+
+
+def conv_norm(x, filters, kernel_size=3, groups=32):
+    x = layers.Conv2D(filters=filters, kernel_size=kernel_size, padding='same', use_bias=False)(x)
+    x = tfa.layers.GroupNormalization(groups=groups, epsilon=1e-5)(x)
+    return x
+
+
+def solov2_head_model(stacked_convs=4, ch_in=258, ch_feature=512, ch_kernel_out=256, num_classes=80):
+    inputs = layers.Input(shape=(None, None, ch_in))
+    feature_kernel = inputs
+    feature_cls = inputs[..., :-2]
+    for _ in range(stacked_convs):
+        feature_kernel = tf.nn.relu(conv_norm(feature_kernel, filters=ch_feature))
+        feature_cls = tf.nn.relu(conv_norm(feature_cls, filters=ch_feature))
+    feature_kernel = layers.Conv2D(filters=ch_kernel_out,
+                                   kernel_size=3,
+                                   padding='same',
+                                   kernel_initializer=tf.random_normal_initializer(stddev=0.01))(feature_kernel)
+    feature_cls = layers.Conv2D(filters=num_classes,
+                                kernel_size=3,
+                                padding='same',
+                                kernel_initializer=tf.random_normal_initializer(stddev=0.01),
+                                bias_initializer=tf.initializers.constant(np.log(1 / 99)))(feature_cls)
+    return tf.keras.Model(inputs=inputs, outputs=[feature_kernel, feature_cls])
+
+
+def solov2_head(P2, P3, P4, P5):
+    head_model = solov2_head_model()
+    # applying maxpool first for P2
+    P2 = layers.MaxPool2D()(P2)
+    features = [P2, P3, P4, P5, P5]
+    grid_sizes = [40, 36, 24, 16, 12]
+    feat_kernel_list, feat_cls_list = [], []
+    for feature, grid_size in zip(features, grid_sizes):
+        feature = pad_with_coord(feature)
+        feature = tf.image.resize(feature, size=(grid_size, grid_size))
+        feat_kernel, feat_cls = head_model(feature)
+        feat_kernel_list.append(feat_kernel)
+        feat_cls_list.append(feat_cls)
+    return feat_cls_list, feat_kernel_list
+
+
+def solov2_maskhead(P2, P3, P4, P5, mid_ch=128, out_ch=256):
+    pdb.set_trace()
+    # first level
+    P2 = tf.nn.relu(conv_norm(P2, filters=mid_ch))
+    # second level
+    P3 = tf.nn.relu(conv_norm(P3, filters=mid_ch))
+    P3 = layers.UpSampling2D()(P3)
+    # third level
+    P4 = tf.nn.relu(conv_norm(P4, filters=mid_ch))
+    P4 = layers.UpSampling2D()(P4)
+    P4 = tf.nn.relu(conv_norm(P4, filters=mid_ch))
+    P4 = layers.UpSampling2D()(P4)
+    # top level, add coordinate
+    P5 = pad_with_coord(P5)
+    P5 = tf.nn.relu(conv_norm(P5, filters=mid_ch))
+    P5 = layers.UpSampling2D()(P5)
+    P5 = tf.nn.relu(conv_norm(P5, filters=mid_ch))
+    P5 = layers.UpSampling2D()(P5)
+    P5 = tf.nn.relu(conv_norm(P5, filters=mid_ch))
+    P5 = layers.UpSampling2D()(P5)
+    seg_outputs = tf.nn.relu(conv_norm(P2 + P3 + P4 + P5, filters=out_ch, kernel_size=1))
+    return seg_outputs
 
 
 def solov2(input_shape, l2_reg=1e-4):
@@ -36,10 +113,15 @@ def solov2(input_shape, l2_reg=1e-4):
     C4 = resnet50.layers[142].output
     assert resnet50.layers[-1].name == "conv5_block3_out"
     C5 = resnet50.layers[-1].output
-    P2, P3, P4, P5, P6 = fpn(C2, C3, C4, C5, l2_reg=l2_reg)
-    pdb.set_trace()
+    P2, P3, P4, P5 = fpn(C2, C3, C4, C5, l2_reg=l2_reg)
+    feat_seg = solov2_maskhead(P2, P3, P4, P5)  # [B, h/4, w/4, 128]
+    feat_cls_list, feat_kernel_list = solov2_head(P2, P3, P4, P5)  # [B, grid, grid, 80], [B, grid, grid, 256]
+    model = tf.keras.Model(inputs=inputs, outputs=[feat_seg, feat_cls_list, feat_kernel_list])
+    return model
 
 
 if __name__ == "__main__":
-
-    solov2(input_shape=(1024, 1024, 3))
+    model = solov2(input_shape=(1024, 1024, 3))
+    x = tf.ones([2, 1024, 1024, 3])
+    feat_seg, feat_cls_list, feat_kernel_list = model(x)
+    pdb.set_trace()
